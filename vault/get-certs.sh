5a94981b27e027aa85bce79342af4c0a3e05e1e0
Tobias Kongsvik <tokongs98@gmail.com>
2021-03-26 13:24:33 +0100

    temp

--- vault/get-certs.sh
+++ vault/get-certs.sh
@@ -1,15 +1,15 @@
-SECRET=$(vault write -format=json pki_int/issue/consul-client common_name=localhost ttl=768h)
+#!/bin/bash
+SECRET=$(vault write -format=json pki_int/issue/consul-node common_name=localhost ttl=768h)
 
 FILE_NAME=$(uuidgen)
 
 touch $FILE_NAME.key
 touch $FILE_NAME.crt
 
-
 echo $SECRET | jq -r '.data.certificate' > $FILE_NAME.crt
 echo $SECRET | jq -r '.data.private_key' > $FILE_NAME.key
 
 openssl pkcs12 -export -in $FILE_NAME.crt -inkey $FILE_NAME.key -out bundle.p12
 
-rm $FILE_NAME.crt
-rm $FILE_NAME.key
+#rm $FILE_NAME.crt
+#rm $FILE_NAME.key
